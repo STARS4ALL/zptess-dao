0d89434e649783c3fe71513e92359ea590c5c55b
Rafael González <astrorafael@gmail.com>
2025-08-29 19:38:56 +0200

    first skeleton of zptess DAO

---
+++ src/zptessdao/__init__.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2022
+#
+# See the LICENSE file for details
+# see the AUTHORS file for authors
+# ----------------------------------------------------------------------
+
+# --------------
+# local imports
+# -------------
+
+from ._version import __version__
+from .constants import (
+    CentralTendency,
+    Calibration,
+)
+
+__all__ = [
+    "__version__",
+    "CentralTendency",
+    "Calibration",
+]
